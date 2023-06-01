f65c5d6f388add1ab905ddf384f9a07e65075495
Vítor Conceição <vitoralmeidavc@outlook.pt>
2023-06-01 13:20:02 +0100

    api

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-let apiLink = "http://mywebapi.somee.com/api"
-
 export const GetEmployees = async () => {
   try {
-    const response = await axios.get(`${apiLink}/Employees`);
+    const response = await axios.get("http://mywebapi.somee.com/api/Employees");
     return response.data;
   } catch (error) {
     console.error(error);
