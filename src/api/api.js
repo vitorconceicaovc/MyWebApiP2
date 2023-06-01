8ecacf8b9dbc4566e1670d0fa457382bab6b8306
Vítor Conceição <vitoralmeidavc@outlook.pt>
2023-06-01 13:27:28 +0100

    api -axios

--- src/api/api.js
+++ src/api/api.js
@@ -2,8 +2,12 @@ import axios from 'axios';
 
 export const GetEmployees = async () => {
   try {
-    const response = await axios.get("http://mywebapi.somee.com/api/Employees");
-    return response.data;
+    const response = await fetch("http://mywebapi.somee.com/api/Employees");
+    if (!response.ok) {
+      throw new Error("Erro na solicitação. Código de resposta: " + response.status);
+    }
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.error(error);
     throw error;
